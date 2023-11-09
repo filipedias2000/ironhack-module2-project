ceac380c9cb0a1b36d04c99a6bc5359ac5d21905
Andre Gomes <andrefilipe.valeriogomes@gmail.com>
2023-11-09 12:58:44 +0000

    Starter code that allows display in Vite

--- react-module2-project/src/App.jsx
+++ react-module2-project/src/App.jsx
@@ -3,16 +3,20 @@ import './App.css'
 import {Routes, Route} from "react-router-dom";
 import HomePage from "./Components/HomePage";
 import ListPage from './Components/ListPage';
+import List from './Components/List';
 
 function App() {
  
 
   return (
     <div>
+      
       <Routes>
         <Route path="/" element={<HomePage/>}/>
-        <Route path="/list" element={<ListPage/>}/>
+        <Route path="/list" element={<List/>}/>
+        <Route path="/list/:bookId" element={<ListPage/>}/>
       </Routes>
+
     </div>
   )
 }
--- react-module2-project/src/Components/List/index.jsx
+++ react-module2-project/src/Components/List/index.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 
 const API_URL = "https://openlibrary.org/api/books?bibkeys=:bookId&jscmd=data&format=json";
 
-function List() {
+function ListPage() {
   const { bookId } = useParams();
   const [bookDetails, setBookDetails] = useState(null);
 
@@ -19,12 +19,24 @@ function List() {
     <div>
       {bookDetails && (
         <div>
+          <img src="${bookDetails.cover.large?}"></img>
           <h2>Title: {bookDetails.title}</h2>
+          <h3>Number of Pages: {bookDetails.number_of_pages}</h3>
+          <h3>Pubish date: {bookDetails.publish_date}</h3>
+          <h3>Publisher: {bookDetails.publishers.map((publisher) => publisher).join()}</h3>
+          
+          <h3>Classification: {bookDetails.classifications.dewey_decimal_class}</h3>
+
+          
+
+
+
+          {/* Display other details as needed */}
+
         </div>
       )}
-
     </div>
   );
 }
 
-export default List;
+export default ListPage;
