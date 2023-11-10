155d679f7c61fae3764c4a0a1b7c4c9909c82919
Filipe Dias <dias.filipe2000gmail.com>
2023-11-10 10:20:56 +0000

    Previous starter version

--- react-module2-project/src/Components/HomePage/index.jsx
+++ react-module2-project/src/Components/HomePage/index.jsx
@@ -2,15 +2,45 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 
-const API_URL = "https://comicvine.gamespot.com/";
+const API_URL = "https://openlibrary.org/works";
 
 function HomePage() {
-    
+
+    const [works, setWorks] = useState([""]);
+
+    const [fetching, setFetching] = useState(true)
+
+    useEffect(()=>{
+        axios.get(API_URL).then((response)=>{
+            setWorks(response.data);
+            setFetching(false); 
+        })
+    }, []);
+
+
 
 
     return(
         <div>
-            <h1>Choose your Character</h1>
+            <h1>Search for a book</h1>
+            {works.map((work)=> 
+            {
+                return(
+                    <div key={`ISBN:${work.ISBN}`}>
+                        {/* <Link to={`/works/`}> */}
+                            {/* <img src={work.image_url} /> */}
+                            <div>
+                                <h3>{work.name}</h3>
+                               
+                            </div>
+                        {/* </Link> */}
+                        <br />
+                    </div>
+                )
+            })
+
+            }
+          
 
         </div>
 
