8de7f1ab1ca4c27825cebd8f33f7ccafe2001f3d
Andre Gomes <andrefilipe.valeriogomes@gmail.com>
2023-11-10 16:58:51 +0000

    small improvments made in terms of clarity

--- react-module2-project/src/Components/BookDetailsPage/DetailsByISBN.jsx
+++ react-module2-project/src/Components/BookDetailsPage/DetailsByISBN.jsx
@@ -4,31 +4,36 @@ import { useParams } from "react-router-dom";
 
 const API_URL = "https://openlibrary.org/api/books?bibkeys=:bookId&jscmd=data&format=json";
 
-function BookDetailsPage() {
+function BookByISBN() {
   const { bookId } = useParams();
-  const [bookDetails, setBookDetails] = useState(null);
+  const [bookDetailsISBN, setBookDetailsISBN] = useState(null);
 
   useEffect(() => {
-    axios.get(API_URL.replace(":bookId", bookId)).then((response) => {
-      setBookDetails(response.data[bookId]);
-    })
-    .catch((error) => console.log(error));
+    axios.get(API_URL.replace(":bookId", bookId))
+      .then((response) => {
+        setBookDetailsISBN(response.data[bookId]);
+      })
+      .catch((error) => {
+        console.error("Error fetching book details:", error);
+        // Handle the error gracefully, e.g., display an error message
+      });
   }, [bookId]);
 
   return (
     <div>
        <h1>Book Details Page</h1>
-      {bookDetails && (
+      {bookDetailsISBN && (
         <div>
-          <img src={bookDetails.cover.large} alt="" height={200} />
-          <h2>Title: {bookDetails.title}</h2>
-          <h3>Number of Pages: {bookDetails.number_of_pages}</h3>
-          <h3>Weight: {bookDetails.weight}</h3>
+          {bookDetailsISBN.cover && bookDetailsISBN.cover.large && (
+            <img src={bookDetailsISBN.cover.large} alt="" height={200} />
+          )}
+          <h2>Title: {bookDetailsISBN.title}</h2>
+          <h3>Number of Pages: {bookDetailsISBN.number_of_pages}</h3>
+          <h3>Weight: {bookDetailsISBN.weight}</h3>
         </div>
       )}
-
     </div>
   );
 }
 
-export default BookDetailsPage;
+export default BookByISBN;
--- react-module2-project/src/Components/NavBar.jsx
+++ react-module2-project/src/Components/NavBar.jsx
@@ -7,9 +7,8 @@ return (
     <nav>
           <ul>
             < Link to = "/">HomePage </Link>
-            < Link to = "/list/:bookId">BookDetailsPage </Link>
             { /*AINDA EM TESTES:*/}
-            < Link to = "/listName/:bookName">BookDetailsbyISBNPage </Link>
+          
            { /*< Link to = "/beers">AllBeersPage </Link>
             < Link to = "/random-beer">RandomBeerPage </Link>
             < Link to = "/new-beer">AddBeerPage </Link>
