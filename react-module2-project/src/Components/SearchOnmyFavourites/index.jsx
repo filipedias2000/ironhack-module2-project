8e4a004b51b5b380b8f8ef9be7863938e2b8e91b
Andre Gomes <andrefilipe.valeriogomes@gmail.com>
2023-11-16 17:51:23 +0000

    latest version working

---
+++ react-module2-project/src/Components/SearchOnmyFavourites/index.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function SearchOnFav({ onSearch }) {
+  const [searchTermName, setSearchTermName] = useState('');
+
+  const handleSearch = (event) => {
+    const value = event.target.value;
+    setSearchTermName(value);
+    onSearch(value); // Pass the search term to the parent component or function
+  };
+
+  const handleSaveName = () => {
+    // Use the 'searchTermName' variable wherever you need it.
+    console.log('Saved search term:', searchTermName);
+    // Perform other actions with the searchTermName variable as needed.
+  };
+
+  return (
+    <div>
+      <label htmlFor="searchInput">Search by Name:</label>
+      <input
+        type="text"
+        id="searchInput"
+        value={searchTermName}
+        onChange={handleSearch}
+        placeholder="Type your search here"
+      />
+      <button onClick={handleSaveName}>
+        <Link style={{textDecoration: 'none', color: 'white'}} to={`/list/personal/${searchTermName}` }>Submit</Link>
+      </button>
+    </div>
+  );
+}
+
+export default SearchOnFav;
